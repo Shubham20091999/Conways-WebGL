ece1a17dfd146ea7f0ca4d521a6da861fc7e3d5d
Shubham <shubham20091999@gmail.com>
2022-02-21 22:57:32 +0530

    moved to utils

--- out/main.js
+++ out/main.js
@@ -1,68 +1,15 @@
 "use strict";
-//Configurations-----
+//Configurations-----------------
 var canvas = document.querySelector("#main");
 const gl = canvas.getContext("webgl2");
-var pxSize = 260;
+const pxSize = 260;
 canvas.width = Math.floor(window.innerWidth / pxSize) * pxSize;
 canvas.height = Math.floor(window.innerHeight / pxSize) * pxSize;
 const width = canvas.width / pxSize;
 const height = canvas.height / pxSize;
 console.log(width, height);
 const shaderLocation = "../shaders/";
-async function getShaders(baseLocation, list) {
-    let ret = Object();
-    for (const [key, value] of Object.entries(list)) {
-        const response = await fetch(baseLocation + value);
-        ret[key] = await response.text();
-    }
-    return ret;
-}
-function initProgram(gl, vertexShaderSource, fragmentShaderSource) {
-    function createShader(gl, type, source) {
-        var shader = gl.createShader(type);
-        gl.shaderSource(shader, source);
-        gl.compileShader(shader);
-        var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-        if (success) {
-            return shader;
-        }
-        console.log(gl.getShaderInfoLog(shader));
-        gl.deleteShader(shader);
-        throw "Shader with type[" + type + "] could not be initialized";
-    }
-    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-        return program;
-    }
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
-    throw "Program could not be initialized";
-}
-function getRandomBitArray(size) {
-    return Array.from({ length: size }, () => (Number(Math.random() > 0.5) * 255));
-}
-//Debug-----------------
-function getTextureData(tex) {
-    var GL = gl;
-    var fb = GL.createFramebuffer();
-    GL.bindFramebuffer(GL.FRAMEBUFFER, fb);
-    GL.framebufferTexture2D(GL.FRAMEBUFFER, GL.COLOR_ATTACHMENT0, GL.TEXTURE_2D, tex, 0);
-    var pixels = new Uint8Array(width * height * 4);
-    if (GL.checkFramebufferStatus(GL.FRAMEBUFFER) == GL.FRAMEBUFFER_COMPLETE) {
-        GL.readPixels(0, 0, width, height, GL.RGBA, GL.UNSIGNED_BYTE, pixels);
-        console.log(pixels);
-        console.log(pixels.filter((value, index, self) => self.indexOf(value) === index));
-    }
-    GL.deleteFramebuffer(fb);
-    return pixels;
-}
-//----------------
+//---------------------------------
 function main(gl, program, displayProgram) {
     gl.useProgram(program);
     gl.viewport(0, 0, width, height);
---
+++ scripts/utils.ts
@@ -0,0 +1,64 @@
+async function getShaders(baseLocation: string, list: Object) {
+    let ret = Object();
+    for (const [key, value] of Object.entries(list)) {
+        const response = await fetch(baseLocation + value);
+        ret[key] = await response.text();
+    }
+    return ret;
+}
+
+function initProgram(gl: WebGL2RenderingContext, vertexShaderSource: string, fragmentShaderSource: string) {
+    function createShader(gl: WebGL2RenderingContext, type: number, source: string) {
+        var shader = gl.createShader(type)!;
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+        var success = gl.getShaderParameter(shader!, gl.COMPILE_STATUS);
+        if (success) {
+            return shader;
+        }
+
+        console.log(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        throw "Shader with type[" + type + "] could not be initialized";
+    }
+
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)!;
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)!;
+
+    var program = gl.createProgram()!;
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+    throw "Program could not be initialized";
+}
+
+
+function getRandomBitArray(size: number) {
+	return Array.from({ length: size }, () => (Number(Math.random() > 0.5) * 255));
+}
+
+
+//Debug=========================
+function getTextureData(GL: WebGL2RenderingContext, texture: WebGLTexture) {
+    var fb = GL.createFramebuffer();
+    GL.bindFramebuffer(GL.FRAMEBUFFER, fb);
+
+    GL.framebufferTexture2D(GL.FRAMEBUFFER, GL.COLOR_ATTACHMENT0, GL.TEXTURE_2D, texture, 0);
+    var pixels = new Uint8Array(width * height * 4);
+    if (GL.checkFramebufferStatus(GL.FRAMEBUFFER) == GL.FRAMEBUFFER_COMPLETE) {
+        GL.readPixels(0, 0, width, height, GL.RGBA, GL.UNSIGNED_BYTE, pixels);
+        console.log(pixels);
+        console.log(pixels.filter((value, index, self) => self.indexOf(value) === index))
+
+    }
+    GL.deleteFramebuffer(fb);
+
+    return pixels;
+}
