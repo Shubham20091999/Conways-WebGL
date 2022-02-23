e78cede93d06bbb1f989e1d6831d67eda007a5a3
Shubham <shubham20091999@gmail.com>
2022-02-23 23:52:28 +0530

    minor bug fixes and performance improvements

--- shaders/computeFrag.fs
+++ shaders/computeFrag.fs
@@ -1,28 +1,28 @@
 #version 300 es
 
-precision highp float;
+precision mediump float;
 
 uniform sampler2D u_texture;
 uniform vec2 u_size;
 
 out vec4 outColor;
 
-vec4 getValueAt(int i, int j) {
-	return texelFetch(u_texture, ivec2(mod((gl_FragCoord.xy) - vec2(i, j), u_size)), 0);
+lowp float getAliveOrDeadAt(int i, int j) {
+	return float(texelFetch(u_texture, ivec2(mod((gl_FragCoord.xy) - vec2(i, j), isAlive u_size)), 0).r > 0.1);
 }
 
-float getAliveOrDeadAt(int i, int j) {
-	return float(getValueAt(i, j).r > 0.4);
+bool approxEqual(lowp float lhs, lowp float rhs) {
+	return (lhs > rhs - 0.01) && (lhs < rhs + 0.01);
 }
 
 void main() {
-	float isAlive = getAliveOrDeadAt(0, 0);
-	float aliveNeighbourCount = getAliveOrDeadAt(1, 1) + getAliveOrDeadAt(1, -1) + getAliveOrDeadAt(-1, 1) + getAliveOrDeadAt(-1, -1) + getAliveOrDeadAt(0, 1) + getAliveOrDeadAt(1, 0) + getAliveOrDeadAt(0, -1) + getAliveOrDeadAt(-1, 0);
+	lowp float isAlive = getAliveOrDeadAt(0, 0);
+	lowp float aliveNeighbourCount = getAliveOrDeadAt(1, 1) + getAliveOrDeadAt(1, -1) + getAliveOrDeadAt(-1, 1) + getAliveOrDeadAt(-1, -1) + getAliveOrDeadAt(0, 1) + getAliveOrDeadAt(1, 0) + getAliveOrDeadAt(0, -1) + getAliveOrDeadAt(-1, 0);
 
-	float willBeAlive = float((aliveNeighbourCount == 3.0) || (isAlive>0.4 && aliveNeighbourCount == 2.0));
+	lowp float willBeAlive = float(approxEqual(aliveNeighbourCount, 3.0) || (isAlive > 0.4 && approxEqual(aliveNeighbourCount, 2.0)));
 
 	//For diming effect for newly alive cell
-	willBeAlive *= (isAlive+0.7);
+	willBeAlive *= (isAlive + 0.9);
 
-	outColor = vec4(willBeAlive,0.0,0.0,1.0);
+	outColor = vec4(willBeAlive, 0.0, 0.0, 1.0);
 }
